b4d8cb1b3ed6a5b4e962824c295bf3a4da8be8e7
Lucas Paulo <lp152659@gmail.com>
2025-05-06 22:29:43 -0300

    Resolução em diretórios do projeto

--- scripts/base/installDeps.sh
+++ scripts/base/installDeps.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd backend || exit
+source ./createStructure.sh
+
+cd $package || exit
 echo "Instalando dependências base..."
 npm install express dotenv cors
 npm install nodemon --save-dev
